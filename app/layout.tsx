fe785b83fede115c2301f99285c0211b06f99d8c
MyeonghoonNam <codenmh0822@naver.com>
2025-09-19 01:43:54 +0900

    style : 스크롤 스타일 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
         />
       </head>
-      <body className="mx-auto max-w-[1920px] overflow-hidden">{children}</body>
+      <body className="mx-auto max-w-[1920px]">{children}</body>
     </html>
   );
 }
