04ab781f21d4ce3ff66753f6101c812012b61de2
MyeonghoonNam <codenmh0822@naver.com>
2025-09-18 18:11:15 +0900

    style : 루트 색상 참조 가능하도록 컴포넌트 개선

--- components/icons/ChevronDown.tsx
+++ components/icons/ChevronDown.tsx
@@ -1,6 +1,6 @@
 import { IconProps } from './types';
 
-export default function ChevronDown({ className, size = 24, color = '#6D6D6D' }: IconProps) {
+export default function ChevronDown({ className, size = 24 }: IconProps) {
   return (
     <svg
       width={size}
@@ -10,7 +10,7 @@ export default function ChevronDown({ className, size = 24, color = '#6D6D6D' }:
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      <path d="M17.5 15.1382L11.5 9.13818L5.5 15.1382" stroke={color} strokeWidth="1.6" />
+      <path d="M17.5 15.1382L11.5 9.13818L5.5 15.1382" stroke="currentColor" strokeWidth="1.6" />
     </svg>
   );
 }
