6299ef89963615c44782e9ca818f370d09aff32b
MyeonghoonNam <codenmh0822@naver.com>
2025-09-22 11:58:03 +0900

    feat : tailwindcss 뷰포트 기반 브레이크 포인트 반환 커스텀 훅 구현

---
+++ hooks/useBreakpoint/index.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+type Breakpoint = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'base';
+
+const breakpoints: Record<Breakpoint, number> = {
+  base: 0,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
+};
+
+export function useBreakpoint(): Breakpoint {
+  const [breakpoint, setBreakpoint] = useState<Breakpoint>('base');
+
+  const getBreakpoint = (width: number): Breakpoint => {
+    if (width >= breakpoints['2xl']) return '2xl';
+    if (width >= breakpoints['xl']) return 'xl';
+    if (width >= breakpoints['lg']) return 'lg';
+    if (width >= breakpoints['md']) return 'md';
+    if (width >= breakpoints['sm']) return 'sm';
+    return 'base';
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBreakpoint(getBreakpoint(window.innerWidth));
+    };
+
+    handleResize(); // 초기 실행
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return breakpoint;
+}
