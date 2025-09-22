85cb3445039c9864c54256be113c2e1701759893
MyeonghoonNam <codenmh0822@naver.com>
2025-09-22 12:52:52 +0900

    style : 섹션 bg 수정

--- components/features/main/sections/MainMemoSection.tsx
+++ components/features/main/sections/MainMemoSection.tsx
@@ -2,7 +2,7 @@ import { MainMemoCarousel } from '../components';
 
 export default function MainMemoSection() {
   return (
-    <section className="flex w-full flex-col justify-center">
+    <section className="flex w-full flex-col justify-center bg-[#F8F1EA]">
       <div className="flex h-[327px] w-full flex-col justify-end gap-3 bg-primary px-5 pb-[120px] pt-20 lg:flex-row lg:items-end lg:justify-between">
         <div className="flex flex-col gap-[10px]">
           <h2 className="font-serif text-2xl font-bold text-white">지금 뜨는 메모</h2>
--- components/features/main/sections/MainSearchSection.tsx
+++ components/features/main/sections/MainSearchSection.tsx
@@ -25,8 +25,8 @@ export default function MainSearchSection() {
   };
 
   return (
-    <div className="flex w-full justify-center">
-      <div className="flex w-[376px] flex-col items-center justify-end gap-8 bg-background px-5 pb-[100px] pt-11 lg:w-[720px] lg:px-0 lg:pt-[72px]">
+    <section className="flex w-full justify-center bg-[#F8F1EA]">
+      <div className="flex w-[376px] flex-col items-center justify-end gap-8 px-5 pb-[100px] pt-11 lg:w-[720px] lg:px-0 lg:pt-[72px]">
         {/* Play Icon Container */}
         <div className="relative h-20 w-20 lg:h-[112px] lg:w-[112px]">
           <Image src="/images/play.webp" alt="Play icon" className="object-contain" priority fill />
@@ -51,6 +51,6 @@ export default function MainSearchSection() {
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
