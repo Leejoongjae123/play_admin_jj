74de78b1caea1895aba2862560a13448100766b8
MyeonghoonNam <codenmh0822@naver.com>
2025-09-22 19:15:50 +0900

    feat : 희곡 리스트  UI 구현

--- components/features/main/sections/index.ts
+++ components/features/main/sections/index.ts
@@ -1,3 +1,5 @@
 export { default as MainSearchSection } from './MainSearchSection';
 export { default as MainMemoSection } from './MainMemoSection';
 export { default as MainAdBannerSection } from './MainAdBannerSection';
+export { default as MainSearchResultSection } from './MainSearchResultSection';
+export { default as MainSearchResultPlaySection } from './MainSearchResultPlaySection';
