c81649a02668974d891a91d2aace1b9dc0025416
MyeonghoonNam <codenmh0822@naver.com>
2025-09-23 16:56:44 +0900

    feat : 희곡 리스트 섹션 구현

--- app/(play)/play/page.tsx
+++ app/(play)/play/page.tsx
@@ -1,3 +1,11 @@
+import { SearchInput } from '@/components/common';
+import { PlaySection } from '@/components/features/play';
+
 export default function PlayPage() {
-  return <section>희곡DB페이지</section>;
+  return (
+    <section className="flex w-full flex-1 flex-col gap-[60px] px-[8%] pt-10">
+      <SearchInput />
+      <PlaySection />
+    </section>
+  );
 }
---
+++ components/features/play/sections/index.ts
@@ -0,0 +1,1 @@
+export { default as PlaySection } from './PlaySection';
