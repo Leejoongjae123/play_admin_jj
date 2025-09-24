4bbd4390b420a2dee76b41851647e946f2365a61
MyeonghoonNam <codenmh0822@naver.com>
2025-09-24 13:35:33 +0900

    feat : CTA route 추가

--- components/features/play/sections/PlayHeroSection.tsx
+++ components/features/play/sections/PlayHeroSection.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 export default function PlayHeroSection() {
+  const router = useRouter();
+
   return (
     <section className="relative -mx-[8vw] flex aspect-[375/262] w-screen max-w-[1920px] items-center justify-center py-[60px] md:aspect-[1440/318] md:py-[80px]">
       <Image src="/images/play-hero.webp" alt="Play Hero" fill className="object-cover" />
@@ -14,7 +19,11 @@ export default function PlayHeroSection() {
           </p>
         </div>
 
-        <Button size="sm" className="text-lg font-semibold">
+        <Button
+          size="sm"
+          className="text-lg font-semibold"
+          onClick={() => router.push('/play/register')}
+        >
           희곡 등록하기
         </Button>
       </div>
