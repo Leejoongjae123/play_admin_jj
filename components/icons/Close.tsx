6d95cfbc5372e273cdce0cea280ea4b7d71e0d03
MyeonghoonNam <codenmh0822@naver.com>
2025-09-24 17:26:55 +0900

    style : 컴포넌트 추상화

--- components/icons/Close.tsx
+++ components/icons/Close.tsx
@@ -1,6 +1,6 @@
 import { IconProps } from './types';
 
-export default function Close({ className, size = 24, color = '#6D6D6D' }: IconProps) {
+export default function Close({ className, size = 24, color }: IconProps) {
   return (
     <svg
       width={size}
@@ -12,7 +12,7 @@ export default function Close({ className, size = 24, color = '#6D6D6D' }: IconP
     >
       <path
         d="M6.66666 25.3333L16 16M16 16L25.3333 6.66666M16 16L6.66666 6.66666M16 16L25.3333 25.3333"
-        stroke={color}
+        stroke={color || 'currentColor'}
         strokeWidth="2"
         strokeLinejoin="round"
       />
