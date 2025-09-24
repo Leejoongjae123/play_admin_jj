addfb093c32d2586862b9622124aa3f30610448f
MyeonghoonNam <codenmh0822@naver.com>
2025-09-24 15:21:40 +0900

    feat : 텍스트 기반 입력 폼 컴포넌트 구현

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,7 +9,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       data-slot="input"
       autoComplete="off"
       className={cn(
-        'h-14 w-full min-w-0 rounded-[4px] border border-input bg-transparent px-5 py-4 text-lg outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        'h-14 w-full min-w-0 rounded-[4px] border border-input bg-transparent px-4 py-3 text-base outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 lg:px-5 lg:py-4',
         'focus-visible:ring-[3px] focus-visible:ring-ring/50',
         'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
         className,
