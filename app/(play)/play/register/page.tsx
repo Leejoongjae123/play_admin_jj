04b086ae228eeca5a05d51b4735faa2066c680a3
MyeonghoonNam <codenmh0822@naver.com>
2025-09-24 20:53:04 +0900

    feat : 희곡 등록 페이지 구현

--- app/(play)/play/register/page.tsx
+++ app/(play)/play/register/page.tsx
@@ -1,3 +1,18 @@
+import { PlayRegisterForm } from '@/components/forms';
+
 export default function PlayRegisterPage() {
-  return <section>희곡DB등록페이지</section>;
+  return (
+    <section className="flex w-full flex-1 flex-col pb-[60px] pt-11 lg:pt-[80px]">
+      <div className="flex flex-col gap-3 border-b border-primary px-[8vw] pb-5">
+        <h1 className="font-serif text-xl font-bold text-primary lg:text-[28px]">희곡 등록하기</h1>
+        <p className="text-gray-1 lg:text-xl">
+          인스크립트 희곡 DB에 등록되지 않은 새로운 작품을 올려주세요. 검토 후 DB에 저장됩니다.
+        </p>
+      </div>
+
+      <div className="flex justify-center px-[8vw] pt-5 lg:pt-11">
+        <PlayRegisterForm />
+      </div>
+    </section>
+  );
 }
