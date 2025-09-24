2bb80aa73de6beedc5c1471827fa370de9b9b6cb
MyeonghoonNam <codenmh0822@naver.com>
2025-09-24 15:22:19 +0900

    feat : 텍스트 기반 입력 폼 컴포넌트 구현

--- components/common/Input/FormInput.tsx
+++ components/common/Input/FormInput.tsx
@@ -12,15 +12,17 @@ export default function FormInput({ label, required, error, className, ...props 
   return (
     <div className="flex w-full flex-col">
       <div className="flex items-center justify-between">
-        <FormLabel className={cn('relative w-20 gap-1 text-gray-3', error && '-top-2.5')}>
+        <FormLabel
+          className={cn('relative w-20 gap-1 text-gray-3 lg:w-40 lg:text-xl', error && '-top-2.5')}
+        >
           {label}
           {required && <span className="text-red">*</span>}
         </FormLabel>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex w-full max-w-[430px] flex-col gap-1">
           <Input
             className={cn(
-              'h-12 bg-orange-4 px-4 py-3 text-base placeholder:text-orange-3',
+              'h-12 bg-orange-4 placeholder:text-orange-3 lg:h-14',
               error && 'border-destructive',
               className,
             )}
