330a486efc8d882735ad3cd8cf331b7121722f89
MyeonghoonNam <codenmh0822@naver.com>
2025-09-25 23:01:45 +0900

    feat : 희곡 상세 페이지 구현

--- app/(play)/play/[playId]/page.tsx
+++ app/(play)/play/[playId]/page.tsx
@@ -1,13 +1,27 @@
-import { PlayDetailSection } from '@/components/features/play/sections';
+import { PlayDetailMemoSection, PlayDetailSection } from '@/components/features/play/sections';
+import { DummyMemos } from '@/models/memo';
 import { DummyPlays } from '@/models/play';
 
-export default async function PlayDetailPage() {
+interface PlayDetailPageParams {
+  playId: string;
+}
+
+interface PlayDetailPageProps {
+  params: PlayDetailPageParams;
+}
+
+export default async function PlayDetailPage({ params }: PlayDetailPageProps) {
+  const { playId } = params;
   // TODO: API 연동
   // const play = await getPlay(playId);
+  // const playMemoList = await getPlayMemoList(playId);
+
+  const playMemoList = DummyMemos.filter((memo) => memo.type === 'play').slice(0, 6);
 
   return (
     <section className="flex w-full flex-1 flex-col pb-[60px] pt-11 lg:pt-[80px]">
       <PlayDetailSection play={DummyPlays[0]} />
+      <PlayDetailMemoSection playId={playId} playMemoList={playMemoList} />
     </section>
   );
 }
--- components/features/play/sections/PlayDetailMemoSection.tsx
+++ components/features/play/sections/PlayDetailMemoSection.tsx
@@ -1,3 +1,42 @@
-export default function PlayDetailMemoSection() {
-  return <div>PlayDetailMemoSection</div>;
+'use client';
+
+import { MemoList, ViewMoreLinkButton } from '@/components/common';
+import { EntryForm } from '@/components/forms';
+import { Memo } from '@/models/memo';
+
+interface PlayDetailMemoSectionProps {
+  playId: string;
+  playMemoList: Memo[];
+}
+
+export default function PlayDetailMemoSection({
+  playId,
+  playMemoList,
+}: PlayDetailMemoSectionProps) {
+  const handleSubmit = () => {
+    // TODO: 메모 submit 로직 구현
+    console.log('submit');
+  };
+
+  return (
+    <section className="flex w-full flex-col gap-5 px-[8vw]">
+      {/* 헤더 */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1.5">
+          <span className="font-serif text-xl font-bold text-gray-1 lg:text-[28px]">메모</span>
+          <span className="font-serif text-xl font-bold text-orange-3 lg:text-[28px]">
+            {playMemoList.length}
+          </span>
+        </div>
+
+        <ViewMoreLinkButton href={`/play/${playId}/memos`} />
+      </div>
+
+      {/* 메모 작성 폼 */}
+      <EntryForm submitButtonText="메모 남기기" onSubmit={handleSubmit} />
+
+      {/* 메모 리스트 */}
+      <MemoList memoList={playMemoList} />
+    </section>
+  );
 }
