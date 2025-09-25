c8b568e7e3014d07a8e17a085cac79e179bed4fe
MyeonghoonNam <codenmh0822@naver.com>
2025-09-25 23:01:27 +0900

    style : textarea resize 스타일 제거

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
     <textarea
       data-slot="textarea"
       className={cn(
-        'aria-invalid:ring-destructive/20 aria-invalid:border-destructive field-sizing-content flex min-h-14 w-full rounded-[4px] border border-input bg-transparent px-4 py-3 text-base outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 lg:px-5 lg:py-4',
+        'aria-invalid:ring-destructive/20 aria-invalid:border-destructive field-sizing-content flex min-h-14 w-full resize-none rounded-[4px] border border-input bg-transparent px-4 py-3 text-base outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 lg:px-5 lg:py-4',
         className,
       )}
       {...props}
