124b450cc81ea86aeea5549e88387b6137dcf93b
MyeonghoonNam <codenmh0822@naver.com>
2025-09-25 14:47:44 +0900

    style : 희곡 상세 메모 섹션 레이아웃 적용

---
+++ components/features/play/sections/PlayDetailMemoSection.tsx
@@ -0,0 +1,3 @@
+export default function PlayDetailMemoSection() {
+  return <div>PlayDetailMemoSection</div>;
+}
--- components/features/play/sections/index.ts
+++ components/features/play/sections/index.ts
@@ -2,3 +2,4 @@ export { default as PlaySection } from './PlaySection';
 export { default as PlayMemoSection } from './PlayMemoSection';
 export { default as PlayHeroSection } from './PlayHeroSection';
 export { default as PlayDetailSection } from './PlayDetailSection';
+export { default as PlayDetailMemoSection } from './PlayDetailMemoSection';
